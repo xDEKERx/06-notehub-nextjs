9daaf589191b093dc05b24edb7f3c03135325e72
Dmytro Deker <prostodeker@gmail.com>
2025-07-13 23:18:47 +0300

    new

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+import css from './Footer.module.css';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className={css.footer}>
+      <div className={css.content}>
+        <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+        <div className={css.wrap}>
+          <p>Developer: Dmytro Solonko</p>
+          <p>
+            Contact us:
+            <Link href="<mailto:student@notehub.app>">
+              prostodeker@gmail.com
+            </Link>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
